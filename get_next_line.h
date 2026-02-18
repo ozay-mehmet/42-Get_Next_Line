8f52771d520a85038c8bfc7ea3c98cb15e9d17a6
Mehmet Özay <mehmetozay45@hotmail.com>
2026-02-18 23:25:45 +0300

    Fonksiyon prototipleri eklendi

--- get_next_line.h
+++ get_next_line.h
@@ -18,13 +18,9 @@
 # endif
 
 # include <stdlib.h>
-# include <unistd.h>
 
 char	*get_next_line(int fd);
-char	*ft_strchr(const char *str, int c);
-char	*ft_trim(char *str);
 size_t	ft_strlen(const char *str);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 
