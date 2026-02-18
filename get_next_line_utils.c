f87dcf0aecd9a0859d1b9d9392e414bb3e5e0edf
Mehmet Özay <mehmetozay45@hotmail.com>
2026-02-18 23:35:48 +0300

    Yardımcı fonksiyonlar eklendi

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -36,23 +36,6 @@ size_t	ft_strlen(const char *str)
 	return (i);
 }
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	unsigned char	*dst;
-	unsigned char	*s;
-	size_t			i;
-
-	i = 0;
-	dst = (unsigned char *)dest;
-	s = (unsigned char *)src;
-	while (i < n)
-	{
-		dst[i] = s[i];
-		i++;
-	}
-	return (dest);
-}
-
 char	*ft_strchr(const char *str, int c)
 {
 	size_t	i;
@@ -72,18 +55,28 @@ char	*ft_strchr(const char *str, int c)
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
-	size_t	len_s1;
-	size_t	len_s2;
+	size_t	i;
+	size_t	j;
 
-	if (!s1 || !s2)
+	if (!s1)
+		s1 = "";
+	if (!s2)
 		return (NULL);
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	str = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!str)
 		return (NULL);
-	ft_memcpy(str, s1, len_s1);
-	ft_memcpy(str + len_s1, s2, len_s2);
-	str[len_s1 + len_s2] = '\0';
+	i = 0;
+	while (i < ft_strlen(s1))
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < ft_strlen(s2))
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
 	return (str);
 }
