79cd38f658385d1307c4c661058a6e7b6ccc23c4
Mehmet Ozay <mozay@k2m03s02.42kocaeli.com.tr>
2026-02-20 17:25:45 +0300

    Projenin son hali eklendi
    Projenin kavramında bulunan (dosyadan okuma, satır ayırma vb.) işlemler ele alındı

--- get_next_line.c
+++ get_next_line.c
@@ -13,102 +13,83 @@
 #include "get_next_line.h"
 #include <unistd.h>
 
-static char	*ft_seperate_line(char *stash)
+static char	*ft_separate_line(char *stash)
 {
-	char	*newstr;
 	int		i;
-	int		j;
+	char	*line;
 
-	if (!stash)
+	if (!stash || !*stash)
 		return (NULL);
 	i = 0;
-	while (stash[i] != '\0' && stash[i] != '\n')
+	while (stash[i] && stash[i] != '\n')
 		i++;
-	newstr = (char *) malloc (i + 1);
-	if (!newstr)
+	if (stash[i] == '\n')
+		i++;
+	line = malloc(i + 1);
+	if (!line)
 		return (NULL);
-	j = 0;
-	while (j < i)
+	i = -1;
+	while (stash[++i] && stash[i] != '\n')
+		line[i] = stash[i];
+	if (stash[i] == '\n')
 	{
-		newstr[j] = stash[j];
-		j++;
+		line[i] = '\n';
+		i++;
 	}
-	newstr[i] = '\0';
-	return (newstr);
+	line[i] = '\0';
+	return (line);
 }
 
 static char	*ft_read(int fd, char *stash)
 {
 	char	*buffer;
+	char	*tmp;
 	int		result;
-	char	*temp;
 
-	if (!stash)
-	{
-		stash = (char *) malloc (1);
-		if (!stash)
-			return (NULL);
-		stash[0] = '\0';
-	}
-	buffer = (char *) malloc (BUFFER_SIZE + 1);
+	buffer = malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
 	result = 1;
 	while (!ft_strchr(stash, '\n') && result > 0)
 	{
 		result = read(fd, buffer, BUFFER_SIZE);
-		if (result == -1)
-		{
-			free(buffer);
-			free(stash);
-			return (NULL);
-		}
-		if (result > 0)
-		{
-			buffer[result] = '\0';
-			temp = ft_strjoin(stash, buffer);
-			if (!temp)
-			{
-				free(buffer);
-				return (stash);
-			}
-			free(stash);
-			stash = temp;
-		}
+		if (result <= 0)
+			break ;
+		buffer[result] = '\0';
+		tmp = ft_strjoin(stash, buffer);
+		free(stash);
+		stash = tmp;
 	}
 	free(buffer);
+	if (result < 0)
+	{
+		free(stash);
+		return (NULL);
+	}
 	return (stash);
 }
 
 static char	*ft_update_stash(char *stash)
 {
+	char	*new;
 	int		i;
 	int		j;
-	char	*newstash;
 
-	if (!stash)
-		return (NULL);
 	i = 0;
-	while (stash[i] != '\0' && stash[i] != '\n')
+	while (stash[i] && stash[i] != '\n')
 		i++;
-	if (stash[i] == '\0')
-	{
-		free(stash);
+	if (!stash[i])
+		return (free(stash), NULL);
+	new = malloc(ft_strlen(stash + i + 1) + 1);
+	if (!new)
 		return (NULL);
-	}
 	i++;
-	newstash = (char *) malloc (ft_strlen(stash + i) + 1);
-	if (!newstash)
-		return (NULL);
 	j = 0;
-	while (stash[i + j] != '\0')
-	{
-		newstash[j] = stash[i + j];
-		j++;
-	}
-	newstash[j] = '\0';
+	while (stash[i])
+		new[j++] = stash[i++];
+	new[j] = '\0';
 	free(stash);
-	return (newstash);
+	return (new);
 }
 
 char	*get_next_line(int fd)
@@ -116,31 +97,23 @@ char	*get_next_line(int fd)
 	static char	*stash;
 	char		*line;
 
-	if (fd < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		if (stash)
+		{
+			free(stash);
+			stash = NULL;
+		}
+		return (NULL);
+	}
+	if (!stash)
+		stash = ft_strdup("");
+	if (!stash)
 		return (NULL);
 	stash = ft_read(fd, stash);
 	if (!stash)
 		return (NULL);
-	line = ft_seperate_line(stash);
+	line = ft_separate_line(stash);
 	stash = ft_update_stash(stash);
 	return (line);
-}
-
-#include <stdio.h>
-#include <fcntl.h>
-int main(){
-	int fd = open("dosya.txt", O_RDONLY);
-	char *line1 = get_next_line(fd);
-	char *line2 = get_next_line(fd);
-	char *line3 = get_next_line(fd);
-	
-	printf("%s\n", line1);
-	printf("%s\n", line2);
-	printf("%s\n", line3);
-	
-	free(line1);
-	free(line2);
-	free(line3);
-	close(fd);
-	return (0);
 }
