519f877bbbc5d736e3e7c7cbcfd971554c6433d0
Mehmet Ozay <mozay@k2m03s02.42kocaeli.com.tr>
2026-02-20 17:28:43 +0300

    Yardımcı fonksiyonlar eklendi
    Uzunluk hesabı için strlen, karakter bulmak için strchr, freelenebilir yer açmak için strdup ve stash ile yeni stringi birleştirmek için strjoin fonksiyonu eklendi

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,71 +12,69 @@
 
 #include "get_next_line.h"
 
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mozay <mozay@student.42kocaeli.com.tr      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/07 18:57:43 by mozay             #+#    #+#             */
-/*   Updated: 2026/01/07 18:57:45 by mozay            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "get_next_line.h"
-
-size_t	ft_strlen(const char *str)
+size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (s && s[i])
 		i++;
 	return (i);
 }
 
-char	*ft_strchr(const char *str, int c)
+char	*ft_strchr(const char *s, int c)
 {
-	size_t	i;
+	int	i;
 
+	if (!s)
+		return (NULL);
 	i = 0;
-	while (str[i])
+	while (s[i])
 	{
-		if (str[i] == (char)c)
-			return ((char *)&str[i]);
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
 		i++;
 	}
-	if ((char)c == '\0')
-		return ((char *)&str[i]);
+	if (c == '\0')
+		return ((char *)&s[i]);
 	return (NULL);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strdup(const char *s)
 {
-	char	*str;
-	size_t	i;
-	size_t	j;
+	char	*dup;
+	int		i;
 
-	if (!s1)
-		s1 = "";
-	if (!s2)
-		return (NULL);
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!str)
+	dup = malloc(ft_strlen(s) + 1);
+	if (!dup)
 		return (NULL);
 	i = 0;
-	while (i < ft_strlen(s1))
+	while (s[i])
 	{
-		str[i] = s1[i];
+		dup[i] = s[i];
 		i++;
 	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*ft_strjoin(char *s1, char const *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		str[i] = s1[i];
 	j = 0;
-	while (j < ft_strlen(s2))
-	{
-		str[i + j] = s2[j];
-		j++;
-	}
-	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
 	return (str);
 }
